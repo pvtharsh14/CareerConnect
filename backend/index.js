793ca8895f6726a6ba8887d21ef058cc30498845
pvtharsh14 <138807117+pvtharsh14@users.noreply.github.com>
2024-09-07 23:07:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app= express();
 
 
  const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','https://frontend-six-gamma-52.vercel.app'],
     credentials:true
 }
 
